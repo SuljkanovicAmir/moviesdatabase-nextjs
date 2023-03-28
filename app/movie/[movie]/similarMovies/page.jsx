9b6169f6a98248f034d88733abc4a700211d59ab
Amir <suljkanovic.amir@hotmail.com>
2023-03-28 20:54:33 +0200

    fix

--- app/movie/[movie]/similarMovies/page.jsx
+++ app/movie/[movie]/similarMovies/page.jsx
@@ -4,7 +4,11 @@ import Link from "next/link";
 
 
 async function fetchSimilar(movie) {
-   
+    const data = await fetch(`https://api.themoviedb.org/3/movie/${movie}/similar?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US`) 
+    const res = await data.json()
+    const resResults = res.results;
+    return resResults;
+
 }
 
 
@@ -13,17 +17,16 @@ async function fetchSimilar(movie) {
 export default async function SimilarMovies( { params }) {
     const { movie } = params
 
-    const data = await fetch(`https://api.themoviedb.org/3/movie/${movie}/similar?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US`) 
-    const res = await data.json()
+    const res = await fetchSimilar(movie);
 
+   
     const imagePath = 'https://image.tmdb.org/t/p/original'
-    console.log(res)
     return (
         <div className="movie-list-div"> 
             <h3>More Like This</h3>
-            {res.results.length > 0 &&
+            {res.length > 0 &&
             <div className="movie-list"> 
-            {res.results.map((similar) => (
+            {res.map((similar) => (
                 <Link key={similar.id}  href={`/movie/${similar.id}`}>  
                     <Image className="poster"src={imagePath + similar.poster_path} priority alt={similar.title} width={500} height={500}/>
                 </Link>
